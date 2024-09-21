5ac946c1a982b7f75439f34ff4b116127a6a605e
Ryan Gamboa <gabe.gamboa05@gmail.com>
2024-09-21 00:53:41 -0700

    figured out how to change grid colour when hovering over with mouse, looked at repo to see correct solution for colouring multiple grids

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ function makeDivs(num) {
     for (let i = 0; i < num; i++) {
         let square = document.createElement("div");
         square.id = "square";
-        square.textContent = "s";
+        square.textContent = "";
         container.appendChild(square);
+
+        // putting this event listener inside works for some reason, is it because of the for loop?
+        square.addEventListener('mouseover', () => { 
+            square.style.backgroundColor = "blue";
+        }) 
+
     }
 };
 
 makeDivs(256);
-
 
